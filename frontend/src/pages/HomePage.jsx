4629fe3347e1d7520d2a8aadba68ffac54bcfeb2
Remi Olateru <remiolateru@gmail.com>
2024-11-13 16:59:35 -0500

    add background to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
+//h- is height
+
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+  return <div className='hero-bg h-screen'>HomePage</div>;
+  
 }
 
 export default HomePage
