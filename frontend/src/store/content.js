39d715f9dcb77349ee46af418969b02daff0ac19
Remi Olateru <remiolateru@gmail.com>
2024-11-17 02:00:32 -0500

    create global content store

---
+++ frontend/src/store/content.js
@@ -0,0 +1,11 @@
+//tv and movie global state
+import { create } from "zustand";
+
+export const useContentStore = create( (set) => ({ 
+    contentType:"movie",
+
+    setContentType: (type) => {set({contentType:type})},
+
+    
+
+}))
